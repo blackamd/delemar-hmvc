180bff1a2521c6b288e921c60e36fd6d57aadcc7
ahmed-12-soliman <ahmed.12.soliman@gmail.com>
2022-10-08 22:11:38 +0200

    update : init website

---
+++ frontend/views/site/aluminum-production.php
@@ -0,0 +1,135 @@
+<!-- Main Content -->
+<div id="main-content" class="site-main clearfix">
+    <div id="content-wrap" class="container">
+        <div id="site-content" class="site-content clearfix">
+            <div id="inner-content" class="inner-content-wrap">
+                <div class="themesflat-spacer clearfix" data-desktop="80" data-mobile="60" data-smobile="60"></div>
+                <div class="themesflat-row equalize sm-equalize-auto clearfix">
+                    <div class="span_1_of_6 bg-f7f">
+                        <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60" data-smobile="60"></div>
+                        <div class="themesflat-content-box clearfix" data-margin="0 10px 0 43px" data-mobilemargin="0 15px 0 15px">
+                            <div class="themesflat-headings style-2 clearfix">
+                                <div class="sup-heading">DELEMAR GROUP</div>
+                                <h2 class="heading font-size-28 line-height-39">ALUMINUM PRODUCTION</h2>
+                                <div class="sep has-width w80 accent-bg margin-top-20 clearfix"></div>
+                                <p class="sub-heading margin-top-33 line-height-24">Our current production capacity is 25,000 tons annually which is expected to massively increase upon the completion of our three new factories within the next 24 months. 80% of our production is consumed in the local market and the remaining 20% are exported to our clients in Europe, Africa, and the Middle East since we meet the standards and quality certifications for exporting aluminum globally. </p>
+                            </div>
+                        </div>
+                        <div class="themesflat-spacer clearfix" data-desktop="56" data-mobile="56" data-smobile="56"></div>
+                    </div>
+                    <div class="span_1_of_6 half-background style-5">
+                    </div>
+                </div>
+                <div class="themesflat-spacer clearfix" data-desktop="39" data-mobile="39" data-smobile="39"></div>
+                <div class="flat-content-wrap style-2 clearfix">
+                    <h5 class="title">SERVICE CONSULTANT</h5>
+                    <p>Through our Design-Build service, we provides a single source of responsibility to the client. We work to develop and understand project expectations and then manage those needs with a customized, qualified design team. Innovation should happen throughout a project. To do that, we focus our efforts on each step of every phase to make the entire process smooth and straightforward. Our proactive, collaborative approach is what uncovers unique ideas and opportunities for improvement before the next phase begins.</p>
+                </div>
+                <div class="themesflat-spacer clearfix" data-desktop="37" data-mobile="35" data-smobile="35"></div>
+                <div class="flat-content-wrap style-2 clearfix">
+                    <div class="item">
+                        <div class="inner">
+                            <div class="themesflat-spacer clearfix" data-desktop="10" data-mobile="10" data-smobile="10"></div>
+                            <h5 class="title">OUR SERVICES INCLUDE:</h5>
+                            <p>We have successfully completed projects in numerous states across the continental United States. Autora Construction Services has a proven track record of:</p>
+                            <div class="themesflat-spacer clearfix" data-desktop="8" data-mobile="8" data-smobile="8"></div>
+                            <div class="themesflat-list has-icon style-1 icon-left size-16 clearfix">
+                                <div class="inner">
+                                    <span class="item">
+                                        <span class="icon"><i class="fa fa-check-circle"></i></span>
+                                        <span class="text">Interior Design Planning</span>
+                                    </span>
+                                </div>
+                            </div><!-- /.themeslat-list -->
+                            <div class="themesflat-list has-icon style-1 icon-left size-16 clearfix">
+                                <div class="inner">
+                                    <span class="item">
+                                        <span class="icon"><i class="fa fa-check-circle"></i></span>
+                                        <span class="text">Colour & Finish Consultations</span>
+                                    </span>
+                                </div>
+                            </div><!-- /.themeslat-list -->
+                            <div class="themesflat-list has-icon style-1 icon-left size-16 clearfix">
+                                <div class="inner">
+                                    <span class="item">
+                                        <span class="icon"><i class="fa fa-check-circle"></i></span>
+                                        <span class="text">Architectural & Custom Lighting</span>
+                                    </span>
+                                </div>
+                            </div><!-- /.themeslat-list -->
+                            <div class="themesflat-list has-icon style-1 icon-left size-16 clearfix">
+                                <div class="inner">
+                                    <span class="item">
+                                        <span class="icon"><i class="fa fa-check-circle"></i></span>
+                                        <span class="text">Glass & Acrylic Specifications</span>
+                                    </span>
+                                </div>
+                            </div><!-- /.themeslat-list -->
+                        </div>
+                    </div><!-- /.item -->
+                    <div class="item">
+                        <div class="themesflat-spacer clearfix" data-desktop="0" data-mobile="0" data-smobile="35"></div>
+                        <div class="themesflat-content-box clearfix" data-margin="0 0 0 11px" data-mobilemargin="0 0 0 0">
+                            <div class="inner pd35 bg-ffc">
+                                <h5 class="title text-white line-height-24 padding-left-7">THIS AWESOME SERVICE BRINGS FULL INTEREST INFORMATION FOR YOUR PURPOSE</h5>
+                                <div class="themesflat-spacer clearfix" data-desktop="16" data-mobile="16" data-smobile="16"></div>
+                                <div class="button-wrap has-icon icon-right size-14">
+                                    <a href="#" class="themesflat-button bg-white color-333 w100 font-weight-400 no-letter-spacing pd26"><span>Download .PDF <span class="icon"><i class="fa fa-file-pdf-o"></i></span></span></a>
+                                </div><!-- /.button-wrap -->
+                                <div class="themesflat-spacer clearfix" data-desktop="6" data-mobile="6" data-smobile="6"></div>
+                                <div class="button-wrap has-icon icon-right size-14">
+                                    <a href="#" class="themesflat-button bg-white color-333 w100 font-weight-400 no-letter-spacing pd26 "><span> Download .DOC <span class="icon"><i class="fa fa-file-word-o"></i></span></span></a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="themesflat-spacer clearfix" data-desktop="37" data-mobile="35" data-smobile="35"></div>
+                <div class="flat-content-wrap style-2 clearfix">
+                    <h5 class="title no-letter-spacing">CONTACT US FOR DESIGN-BUILD SERVICES TODAY</h5>
+                    <div class="themesflat-spacer clearfix" data-desktop="27" data-mobile="27" data-smobile="27"></div>
+                    <div class="themesflat-tabs style-2 title-w170 clearfix">
+                        <ul class="tab-title clearfix ">
+                            <li class="item-title active">
+                                <span class="inner">Our focus</span>
+                            </li>
+                            <li class="item-title">
+                                <span class="inner">Dedicated</span>
+                            </li>
+                            <li class="item-title">
+                                <span class="inner">Committed</span>
+                            </li>
+                        </ul>
+
+                        <div class="tab-content-wrap clearfix">
+                            <div class="tab-content">
+                                <div class="item-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                                </div>
+                            </div><!-- /.tab-content -->
+                            <div class="tab-content">
+                                <div class="item-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                                </div>
+                            </div><!-- /.tab-content -->
+                            <div class="tab-content">
+                                <div class="item-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                                </div>
+                            </div><!-- /.tab-content -->
+                        </div><!-- /.tab-content-wrap -->
+                    </div><!-- /.themesflat-tabs -->
+                </div>
+                <div class="themesflat-spacer clearfix" data-desktop="80" data-mobile="60" data-smobile="60"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /#main-content -->
+
+
+
---
+++ frontend/views/site/page-factories.php
@@ -0,0 +1,110 @@
+<?php
+
+use yii\helpers\Url;
+
+?>
+
+<!-- Main Content -->
+<div id="main-content" class="site-main clearfix">
+    <div id="content-wrap">
+        <div id="site-content" class="site-content clearfix">
+            <div id="inner-content" class="inner-content-wrap">
+               <div class="page-content">
+                    <!-- SERVICES -->
+                    <div class="row-services">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="themesflat-spacer clearfix" data-desktop="81" data-mobile="60" data-smobile="60"></div>
+                                    <div class="themesflat-carousel-box data-effect clearfix" data-gap="30" data-column="2" data-column2="2" data-column3="1" data-auto="true">
+                                        <div class="owl-carousel owl-theme">
+                                            <div class="themesflat-image-box style-2 clearfix">
+                                                <div class="image-box-item">
+                                                    <div class="inner">
+                                                        <div class="thumb data-effect-item">
+                                                            <img src="<?= Url::to(['../frontend/web/assets_tamplet/img/imagebox/Secondary vertical logo-01.png'])?>" alt="Image">
+                                                            <div class="overlay-effect bg-color-accent"></div>
+                                                        </div>
+                                                        <div class="text-wrap">
+                                                            <h5 class="heading"><a href="#">ALUMINUM COATING</a></h5>
+                                                            <p class="letter-spacing-01">Within the Delmar factory is a complete painting line for normal wood colors and a fully [...]</p>
+                                                            <div class="elm-readmore">
+                                                                <a href="<?= Url::to(['site/aluminum-coating'])?>">DETAILS</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="themesflat-image-box style-2 clearfix">
+                                                <div class="image-box-item">
+                                                    <div class="inner">
+                                                        <div class="thumb data-effect-item">
+                                                            <img src="<?= Url::to(['../frontend/web/assets_tamplet/img/imagebox/Secondary vertical logo.png'])?>" alt="Image">
+                                                            <div class="overlay-effect bg-color-accent"></div>
+                                                        </div>
+                                                        <div class="text-wrap">
+                                                            <h5 class="heading"><a href="#">ALUMINUM DIE SHOP</a></h5>
+                                                            <p class="letter-spacing-01">Delmar owns and operates a die-cast factory for the production of molds used in the pressing [...]</p>
+                                                            <div class="elm-readmore">
+                                                                <a href="<?= Url::to(['site/aluminum-die-shop'])?>">DETAILS</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><!-- /.themesflat-carousel-box -->
+                                    <div class="themesflat-spacer clearfix" data-desktop="15" data-mobile="15" data-smobile="15"></div>
+                                    <div class="themesflat-carousel-box data-effect clearfix" data-gap="30" data-column="2" data-column2="2" data-column3="1" data-auto="true">
+                                        <div class="owl-carousel owl-theme">
+                                            <div class="themesflat-image-box style-2 clearfix">
+                                                <div class="image-box-item">
+                                                    <div class="inner">
+                                                        <div class="thumb data-effect-item">
+                                                            <img src="<?= Url::to(['../frontend/web/assets_tamplet/img/imagebox/Secondary vertical logo-012.png'])?>" alt="Image">
+                                                            <div class="overlay-effect bg-color-accent"></div>
+                                                        </div>
+                                                        <div class="text-wrap">
+                                                            <h5 class="heading"><a href="#">ALUMINUM PRODUCTION</a></h5>
+                                                            <p class="letter-spacing-01">Our current production capacity is 25,000 tons annually which is expected to massively increase [...]</p>
+                                                            <div class="elm-readmore">
+                                                                <a href="<?= Url::to(['site/aluminum-production'])?>">DETAILS</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="themesflat-image-box style-2 clearfix">
+                                                <div class="image-box-item">
+                                                    <div class="inner">
+                                                        <div class="thumb data-effect-item">
+                                                            <img src="<?= Url::to(['../frontend/web/assets_tamplet/img/imagebox/Secondary vertical logo-013.png'])?>" alt="Image">
+                                                            <div class="overlay-effect bg-color-accent"></div>
+                                                        </div>
+                                                        <div class="text-wrap">
+                                                            <h5 class="heading"><a href="#">PROCESSED GLASS</a></h5>
+                                                            <p class="letter-spacing-01">Our factory is an iconic facility in the glass industry. The automated glass handling systems [...]</p>
+                                                            <div class="elm-readmore">
+                                                                <a href="<?= Url::to(['site/processed-glass'])?>">DETAILS</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><!-- /.themesflat-carousel-box -->
+                                    <div class="themesflat-spacer clearfix" data-desktop="45" data-mobile="60" data-smobile="60"></div>
+                                </div><!-- /.col-md-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /.container -->
+                    </div>
+                    <!-- END SERVICES -->
+               </div><!-- /.page-content -->
+            </div><!-- /#inner-content -->
+        </div><!-- /#site-content -->
+    </div><!-- /#content-wrap -->
+</div><!-- /#main-content -->
+<a id="scroll-top"></a>
+
+
+
