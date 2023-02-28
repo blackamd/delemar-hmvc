9f7d4901f453e5f0bb8a3e3228c564fcc5a83a01
ahmed-12-soliman <ahmed.12.soliman@gmail.com>
2023-02-28 08:06:44 +0200

    update: requirment for dmarab

---
+++ dmarab/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace dmarab;
+
+/**
+ * dmarab module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'dmarab\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
